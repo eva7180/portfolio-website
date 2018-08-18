4ffd2ecb4d8b96a7c4d38fc644c8fc2e8836b125
Eva Stolz <evasmail@gmail.com>
2018-08-18 18:38:46 +0200

    Created routes and placeholder views for all pages.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,14 @@ class PagesController < ApplicationController
 
 	def welcome
 	end
+
+	def portfolio
+	end
+
+	def about
+	end
+
+	def contact
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#welcome'
+
+  get '/portfolio' => 'pages#portfolio'
+
+  get '/about' => 'pages#about'
+
+  get '/contact' => 'pages#contact'
+  
 end
